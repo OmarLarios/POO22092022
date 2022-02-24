29e5883775d81ab44c113da32cb1cab189903dd3
OmarLarios <omarlarios02@aragon.unam.mx>
2022-02-24 12:40:29 -0600

    Iniciamos el arichco foco.java

---
+++ Clases/ConceptosBásicos/src/ico/fes/Foco.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author omarl
+ */
+public class Foco {
+    private String color;
+    private String tipo;
+    private int tam;
+
+    public Foco() {
+    }
+
+    public Foco(String color, String tipo, int tam) {
+        this.color = color;
+        this.tipo = tipo;
+        this.tam = tam;
+    }
+
+    public int getTam() {
+        return tam;
+    }
+
+    public void setTam(int tam) {
+        this.tam = tam;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+}
